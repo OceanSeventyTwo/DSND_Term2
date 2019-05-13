3f5f85cd7c7255b743e7744fbb92286fe3f01d00
OceanSeventyTwo <Thomas.M.Canty@gmail.com>
2019-05-13 16:12:58 -0400

    Completed Plotly Excersize

--- lessons/WebDevelopment/FrontEndWorkspaceFiles/6_exercise_plotly/javascript_files/plot1.js
+++ lessons/WebDevelopment/FrontEndWorkspaceFiles/6_exercise_plotly/javascript_files/plot1.js
@@ -27,6 +27,10 @@ var country_name_brazil = 'Brazil';
 
 var trace1 = {
 /* TODO: Use the year, arable_land_brazil, and country_name_brazil to create a trace for a line chart */
+  x: year,
+  y: arable_land_brazil,
+  name: country_name_brazil,
+
 };
 
 var arable_land_germany = [49.67917502148379,
@@ -54,6 +58,9 @@ var arable_land_germany = [49.67917502148379,
 var country_name_germany = 'Germany';
 var trace2 = {
 /* TODO: Create another trace for the Germany data using a line chart */
+  x:  year,
+  y: arable_land_germany,
+  name: country_name_germany,
 };
 
 
@@ -81,6 +88,9 @@ var arable_land_china = [55.6902039146848,
 var country_name_china = 'China';
 var trace3 = {
 /* TODO: Create another trace for the Germany data using a line chart */
+  x: year,
+  y: arable_land_china,
+  name: country_name_china,
 };
 
 
